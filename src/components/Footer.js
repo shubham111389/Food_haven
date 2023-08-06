87c1ce83503a8057ae7ea9f344c9386263ac396e
shubham annpurne <shubhamannpurne1029@gmail.com>
2023-08-06 20:15:45 +0530

    Add the files

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+// Footer component for footer section
+const Footer = () => {
+  return (
+    <div className="footer">
+      Created By
+      <i className="fa-solid fa-heart"></i>
+      <a href="https://www.linkedin.com/in/shubham-annpurne-0183bb232/" target="_blank" title="Chetan Nada's Linkedin Profile">
+        Shubham Annpurne
+      </a>
+      <i className="fa-solid fa-copyright"></i>2023
+      <strong>
+        Food<span>Haven</span>
+      </strong>
+    </div>
+  );
+};
+
+export default Footer;
