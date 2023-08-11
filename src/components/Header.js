aed11a954077bce68f71bc0d984a649cd05f6225
shubham annpurne <shubhamannpurne1029@gmail.com>
2023-08-11 13:52:05 +0530

    change the carticon styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,14 @@
 import { useState } from "react";
 import FoodHavenLogo from "../images/applogo.png";
 import { Link } from "react-router-dom"; // imported Link for client side routing
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";// used for the  allows programmatic routing inside a React application. 
 import { useSelector } from "react-redux";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartPlus} from '@fortawesome/free-solid-svg-icons';
+
 
 
 // Title component for display logo
-const Title = () => (
-  <div className="flex items-center">
-    <Link to="/">
-      <h2 className="text-xl font-bold text-gray-500 mx-2">
-        Food<span className="text-orange-600">Haven</span>
-      </h2>
-    </Link>
-
-    <img
-      className= "w-26  p-3 "
-      alt="logo"
-      src="https://cdn-icons-png.flaticon.com/512/683/683071.png"
-    />
-  </div>
-);
 
 
 // Header component for header section: Logo, Nav Items
@@ -35,30 +23,25 @@ const Header = () => {
 
 
   return (
-    <div className="header">
-      <Title />
-      <div className="nav-items">
-        <ul>
-          <li className="px-6 text-medium font-semibold flex items-center text-gray-600 hover:text-orange-600">
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <i className="fa-solid fa-cart-shopping"></i>
-          </li>
-            <li className="px-2" data-testid="cart">
-             <Link to="/cart">
-               Cart- {cartItems.length} items
-              </Link>
-            </li>
-
-          <li>
+    <div className="flex  items-center justify-between px-6 md:px-8 py-2 shadow bg-black text-white ">
+    <Link to="/">
+      <h2 className="text-xl font-bold text-gray-500 mx-2">
+        Food<span className="text-orange-600">Haven</span>
+      </h2>
+    </Link>
+     <img
+      className= " py-0 h-14"
+      alt="logo"
+      src="https://cdn-icons-png.flaticon.com/512/683/683071.png"
+    />
+      
+    
+    <ul className="flex gap-6 md:gap-12 text-sm font-medium">
+        <li><Link to="/" className=" hover:text-orange-400  transition-all duration-300 ease-in-out">Home</Link></li>
+        <li><Link to="/about" className=" hover:text-orange-400 transition-all duration-300 ease-in-out">About</Link></li>
+        <li><Link to="/contact" className=" hover:text-orange-400 transition-all duration-300 ease-in-out">Contact</Link></li>
+        <li><Link to="/cart" className="relative "><FontAwesomeIcon icon={faCartPlus} /><span className="absolute top-[-8px] right-[-12px] bg-orange-500 w-4 p-1  h-4 rounded-full text-[10px] flex justify-center items-center" data-testid="cart">{cartItems.length}</span></Link></li>
+        <li>
             {/* use conditional rendering for login and logout */}
             {isLoggedin ? (
               <button
@@ -76,9 +59,10 @@ const Header = () => {
               </button>
             )}
           </li>
+        
         </ul>
       </div>
-    </div>
+    
   );
 };
 
