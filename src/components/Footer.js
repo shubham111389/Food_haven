3a5a3c9d4b6164047ca704cfe7d5f3e3ab150b2f
shubham annpurne <shubhamannpurne1029@gmail.com>
2023-08-12 15:24:17 +0530

    add the syles in footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,151 +1,55 @@
 // Footer component for footer section
 
+import Footer1 from "./FooterHelper";
 
 
-import React from "react";
- 
-const Contact1 = () => {
+const Footer = () => {
+  
   return (
     
-    <div className="py-2 text-fuchsia-500 px-25   ">
-      Created By
-      <i className="fa-solid fa-heart cursor-pointer"></i>
+    
+    <div className="flex-col bg-black">
+      <div>
+      <Footer1/>
+      </div>
+    <div className=" bg-black mx-96 ">
+     <div className=" bg-black text-white  ">
+      <strong className="opacity-50">
+           Created  By 
+           </strong>
+      <i className="fa-solid fa-heart cursor-pointer px-1"></i>
       <a
         href="https://www.linkedin.com/in/shubham-annpurne-0183bb232/"
         target="_blank"
-        title="Chetan Nada's Linkedin Profile"
+        title="Shubham Annpurne's Linkedin Profile"
+        className="font-medium opacity-100"
       >
         Shubham Annpurne
       </a>
-      <i className="fa-solid fa-copyright"></i>2023
-      <strong>
-        Food<span>Haven</span>
-      </strong>
-    </div>
-  );
-};
+      <i className="fa-solid fa-copyright px-1 opacity-50"></i><strong className="opacity-50">
+           @ 2023
+           </strong>
+      <strong className="px-2 font-medium opacity-100 cursor-pointer">
+         Food <span className="px-1"> Haven </span>
+       </strong>
+      
+      
 
 
-const Footer1 = () => {
-  
-  return (
-    <>
-  <div className="grid md:grid-rows-2 ">
-  <div className="relative top-16 flex flex-col justify-between border border-black md:flex md:flex-row py-10 text-white bg-black ">
-      <div className="flex flex-1 justify-around">
-        <div className="leftFooter1">
-          <span className="opacity-50 font-medium">COMPANY</span>
-          <ul className="mt-4 flex flex-col gap-2">
-            <a href="">
-              <li className="footerItem">About us</li>
-            </a>
-            <a href="">
-              <li className="footerItem">Team</li>
-            </a>
-            <a href="">
-              <li className="footerItem">Careers</li>
-            </a>
-            <a href="">
-              <li className="footerItem">Swiggy Blog</li>
-            </a>
-            <a href="">
-              <li className="footerItem">Bug Bounty</li>
-            </a>
-            <a href="">
-              <li className="footerItem">Swiggy One</li>
-            </a>
-            <a href="">
-              <li className="footerItem">Swiggy Corporate</li>
-            </a>
-            <a href="">
-              <li className="footerItem">Swiggy Instamart</li>
-            </a>
-            <a href="">
-              <li className="footerItem">Swiggy Genie</li>
-            </a>
-          </ul>
-        </div>
-        <div className="leftFooter2">
-          <span className="opacity-50 font-medium">CONTACT</span>
-          <ul className="mt-4 flex flex-col gap-2">
-            <a href="">
-              <li className="footerItem">Help & Support</li>
-            </a>
-            <a href="">
-              <li className="footerItem">Partern with us</li>
-            </a>
-            <a href="">
-              <li className="footerItem">Ride with us</li>
-            </a>
-          </ul>
-        </div>
-      </div>
-      <div className="flex flex-1 justify-around md:mt-0 mt-10">
-        <div className="hidden md:block rightFooter1">
-          <span className="opacity-50 font-medium">LEGAL</span>
-          <ul className="mt-4 flex flex-col gap-2">
-            <a href="">
-              <li className="footerItem">Terms & Conditions</li>
-            </a>
-            <a href="">
-              <li className="footerItem">Refund & Cancellation</li>
-            </a>
-            <a href="">
-              <li className="footerItem">Privacy Policy</li>
-            </a>
-            <a href="">
-              <li className="footerItem">Cookie Policy</li>
-            </a>
-            <a href="">
-              <li className="footerItem">Offer Terms</li>
-            </a>
-            <a href="">
-              <li className="footerItem">Phishing & Fraud</li>
-            </a>
-          </ul>
-        </div>
-        
-        <div className="flex flex-col gap-4">
-          <img
-          className="h-12 cursor-pointer"
-            alt=""
-            src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_200,h_65/icon-AppStore_lg30tv"
-          />
-          <img
-          className="h-12 cursor-pointer"
-            alt=""
-            src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_200,h_65/icon-GooglePlay_1_zixjxl"
-          />
-        </div>
-        
      
-        
-      </div>
-     
-
     </div>
-  
-    <Contact1/>
+    </div>
+    </div>
     
-
+  );
+    
+    
+    
+  
+  
+};
+    
+    
  
-
-    </div>
-
-    
-   
-    </>
-    
-    
-
-    
-    
-    
-    
-    
-    
-    
-  );
-};
-export default Footer1;
+export default Footer;
 
